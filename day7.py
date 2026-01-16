7e2c2c31060768fff53fe150213d2c93692d6020
Utkirbek22 <Socialmedia05112022@gmail.com>
2026-01-16 19:53:30 +0100

    day7, greet function is created with 3 prints

---
+++ day7.py
@@ -0,0 +1,6 @@
+def greet():
+    print("I am feeling light headache!")
+    print("no one gonna believe in you unless you do")
+    print("when people look at you, believe in you, stay confident")
+
+greet()
