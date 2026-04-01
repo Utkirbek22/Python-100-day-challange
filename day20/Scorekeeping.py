b2d954bcc9d40372483078cd509d331541dac6be
Utkirbek22 <Socialmedia05112022@gmail.com>
2026-04-01 18:20:45 +0200

    a little bit changes in snake game in Scoreboaed and uncesserty files, no need to change it

--- day20/Scorekeeping.py
+++ day20/Scorekeeping.py
@@ -4,20 +4,32 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.count = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 430)
         self.write(f"Score: {self.count}", align="center", font=("Arial", 24, "normal") )
         self.hideturtle()
 
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.count} High Score: {self.high_score} ", align="center", font=("Arial", 24, "normal"))
     def increase(self):
         self.count += 1
         self.clear()
         self.write(f"Score: {self.count}", align="center", font=("Arial", 24, "normal") )
+    def reset(self):
+        if self.count > self.high_score:
+            self.high_score = self.count
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game over", align="center", font=("Arial", 24, "normal"))
+        self.count = 0
+        self.update_score()
+
+
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("Game over", align="center", font=("Arial", 24, "normal"))
 
 
 
