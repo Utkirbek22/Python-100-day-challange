f093638dc521a6bcfd4d85fd2be53416f6bca649
Utkirbek22 <Socialmedia05112022@gmail.com>
2026-04-10 21:10:27 +0200

    trying to learning

---
+++ day26/main.py
@@ -0,0 +1,30 @@
+numbers  = [1,2,3]
+# new_num = []
+#
+# for i in numbers:
+#     res = i + 3
+#     new_num.append(res)
+# print(new_num)
+
+# List comprehension
+
+# new_list = [i + 10 for i in numbers]
+# print(new_list)
+
+
+# my_nums = [num + 5 for num in numbers]
+# print(my_nums)
+
+# name = "Jack"
+# names = [letter for letter in name]
+# print(names)
+#
+# new_num = [num * 2 for num in numbers]
+#
+# print(new_num)
+#
+
+
+names = ["Jack", "Utkirbek", "Lili","Lolo", "Alis"]
+
+short_name = []
