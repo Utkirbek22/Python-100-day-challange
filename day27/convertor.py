4d0d34f51fd24b377635d158c42ef9b4eeb8439f
Utkirbek22 <Socialmedia05112022@gmail.com>
2026-04-13 16:54:12 +0200

    miles to km convertor app is finished

---
+++ day27/convertor.py
@@ -0,0 +1,43 @@
+from tkinter import *
+
+def km_to_mile():
+    miles = float(miles_input.get())
+    km = miles * 1.609
+    km_number_label.config(text=f"{km}")
+
+
+
+window = Tk()
+window.minsize(width=600, height=400)
+window.title("Miles to km convertor")
+window.config(padx=20, pady=20)
+
+miles_input = Entry()
+miles_input.grid(column=1, row=0)
+
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+equel_to_label = Label(text="is equal to")
+equel_to_label.grid(column=0, row=1)
+
+km_number_label = Label(text="0")
+km_number_label.grid(column=1, row=1)
+
+km_label = Label(text="km")
+km_label.grid(column=2, row=1)
+
+calc_button = Button(text="Calculate", command=km_to_mile)
+calc_button.grid(column=1,row=2)
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
---
+++ day27/repeat.py
@@ -0,0 +1,64 @@
+from tkinter import *
+import tkinter
+
+from day27.main import window
+
+# window = tkinter.Tk()
+# window.minsize(width=1500, height=1200)
+# window.title("Focus on Priority")
+# window.text = "react to nothing"
+#
+# #  label
+#
+# label = tkinter.Label(text="think about time and family, responsibilty")
+#
+# label.config(text="let's focus")
+# print(label)
+#
+#
+# # button
+#
+#
+# def clicked():
+#     print("i got clicked")
+#     new_text = input.get()
+#     label.config(text=new_text)
+#
+# button = Button(text="click me", command=clicked)
+# # input
+#
+# input = Entry()
+# print(input.get())
+#
+# input.pack()
+# button.pack()
+# label.pack()
+# window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# window = tkinter.Tk()
+
+
+
+
+
+
+
+
+
+
+
+
