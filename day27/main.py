e58b916192f2af2429cbb640d5915bd484919c5d
Utkirbek22 <Socialmedia05112022@gmail.com>
2026-04-13 16:29:52 +0200

    button, input skills learnt

--- day27/main.py
+++ day27/main.py
@@ -1,18 +1,36 @@
+from tkinter import *
 import tkinter
 
 window = tkinter.Tk()
 window.title("my first GUI program")
 window.minsize(width=1000, height=700)
 
+#  label
 my_label = tkinter.Label(text="here is your label !", font=("Arial", 24, "italic"))
 my_label.pack()
 
+my_label.config(text="here is new one")
+my_label["text"] = "New text"
+# er
 
 
+# button
+
+def get_clicked():
+    print("I got clikced")
+    new_text = input.get()
+    my_label.config(text=new_text)
+button = Button(text= "click me", command=get_clicked)
+button.pack()
 
 
+# input
 
+input = Entry()
 
+print(input.get())
+
+input.pack()
 
 
 
