466b9ddac86a06b405db05b22b4272ff3b5a2774
Utkirbek22 <Socialmedia05112022@gmail.com>
2026-04-15 17:15:09 +0200

    after the break, i still continue because i will never give up and try and try, and try until i achieve it

--- day29/main.py
+++ day29/main.py
@@ -5,11 +5,20 @@ from PIL import Image,ImageTk
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = entry_website.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt","a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        entry_website.delete(0,END)
+        password_entry.delete(0,END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
-# window.minsize(width=1000,height=800)
+window.config(pady=100, padx=200)
 window.title("Password manager")
 
 
@@ -19,8 +28,36 @@ canva = Canvas(width=1000,height=800)
 img = Image.open("logo.png")
 img = img.resize((400,400))
 lock_img = ImageTk.PhotoImage(img)
-canva.create_image(500,350,image=lock_img)
-canva.pack()
+canva.create_image(500,300,image=lock_img)
+
+canva.grid(column=0,row=0, columnspan=3)
+
+# Label
+website_label = Label(text="Website")
+website_label.grid(column=0,row=1)
+email_label = Label(text="Email/Username")
+email_label.grid(column=0,row=2)
+password_label = Label(text="Password")
+password_label.grid(column=0, row=3)
+
+# Entry
+entry_website = Entry(width=35)
+entry_website.grid(column=1,row=1,columnspan=2)
+entry_website.focus()
+email_entry = Entry(width=35)
+email_entry.grid(column=1,row=2, columnspan=2)
+email_entry.insert(0,"utkirbbeekk@gmail.com")
+password_entry = Entry(width=21)
+password_entry.grid(column=1,row=3)
+
+#  Button
+generate_password_entry = Button(text="Generate")
+generate_password_entry.grid(column=2,row=3)
+add_button = Button(text="Add", command=save)
+add_button.grid(row=4,column=1, columnspan=2)
+
+
+
 
 
 window.mainloop()
