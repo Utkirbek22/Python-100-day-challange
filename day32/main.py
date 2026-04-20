2fe9604e4ee9346261a437f0102da9f9dfbe8934
Utkirbek22 <Socialmedia05112022@gmail.com>
2026-04-20 21:23:03 +0200

    my first email is sent

---
+++ day32/main.py
@@ -0,0 +1,12 @@
+import smtplib
+
+my_email = "Socialmedia05112022@gmail.com"
+password = "nxnzruzsaxgmnqti"
+
+connection = smtplib.SMTP("smtp.gmail.com",587)
+connection.starttls()
+connection.login(user=my_email,password=password)
+
+connection.sendmail(from_addr=my_email,to_addrs="utkirbbeekk@gmail.com", msg="Hello Utkirbek,"
+                                                                             " that is your first email messgae from yoursel, congrats, you are improving your tech skills")
+connection.close()
