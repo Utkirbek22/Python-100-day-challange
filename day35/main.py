249bec4bce207cf8e5c139b5362773a4ac67ae3f
Utkirbek22 <Socialmedia05112022@gmail.com>
2026-04-25 20:54:56 +0200

    day 35, I am trying to make rainy and rainy

---
+++ day35/main.py
@@ -0,0 +1,29 @@
+import requests
+
+api_key = "0ea7ac9a261c5a93471a2cbec0f6050a"
+parametrs = {
+    "lat": 52.239018,
+    "lon": 21.020176,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+
+api_key = "0ea7ac9a261c5a93471a2cbec0f6050a"
+
+# https://api.openweathermap.org/data/2.5/weather?lat=52.239018&lon=21.020176&appid=0ea7ac9a261c5a93471a2cbec0f6050a
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parametrs)
+print(response.status_code)
+data = response.json()
+print(data)
+will_rain = False
+for hour_data in data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("bring umbrella")
+
+
